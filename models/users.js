f6ac57e8679490942b82fdcf0ba7e3215090ad6b
ZaidAsif <zaid.asif.9082@gmail.com>
2025-01-25 15:32:08 +0500

    added isAmin in user schema

--- models/users.js
+++ models/users.js
@@ -3,7 +3,8 @@ import mongoose from 'mongoose';
 const usersSchema = new mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
-    emailVerified: {type: Boolean, required: true, default: false}
+    emailVerified: {type: Boolean, required: true, default: false},
+    isAdmin: {type: Boolean, required: true, default: false}
 })
 
 const Users = mongoose.model("Users", usersSchema);
